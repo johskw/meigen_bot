4086da654a1a1bb2f9060bd70b132cb962033f14
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-04-03 15:07:51 +0900

    use basic auth

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/johskw/meigen_bot/router"
 )
 
 func main() {
+	err := godotenv.Load("bot.env")
+	if err != nil {
+		log.Fatal(err)
+	}
 	r := gin.Default()
 	r.LoadHTMLGlob("app/templates/*.tmpl")
 	router.SetRoutes(r)
--- router/router.go
+++ router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/johskw/meigen_bot/handler"
 )
@@ -8,20 +10,24 @@ import (
 func SetRoutes(r *gin.Engine) {
 	r.POST("/callback", handler.Callback)
 
-	r.GET("/", handler.ShowIndex)
-	r.GET("/character/:id", handler.ShowCharacter)
-	r.GET("/newcharacter", handler.ShowNewCharacterForm)
-	r.POST("/createcharacter", handler.CreateCharacter)
-	r.GET("/editcharacter/:id", handler.ShowEditCharacterForm)
-	r.POST("/updatecharacter/:id", handler.UpdateCharacter)
-	r.POST("/deletecharacter/:id", handler.DeteleCharacter)
+	auth := r.Group("/", gin.BasicAuth(gin.Accounts{
+		os.Getenv("AUTH_USER"): os.Getenv("AUTH_PATH"),
+	}))
 
-	r.POST("/createnickname/:character_id", handler.CreateNickname)
-	r.POST("/deletenickname/:nickname_id/:character_id", handler.DeleteNickname)
+	auth.GET("/", handler.ShowIndex)
+	auth.GET("/character/:id", handler.ShowCharacter)
+	auth.GET("/newcharacter", handler.ShowNewCharacterForm)
+	auth.POST("/createcharacter", handler.CreateCharacter)
+	auth.GET("/editcharacter/:id", handler.ShowEditCharacterForm)
+	auth.POST("/updatecharacter/:id", handler.UpdateCharacter)
+	auth.POST("/deletecharacter/:id", handler.DeteleCharacter)
 
-	r.POST("/createmeigen/:character_id", handler.CreateMeigen)
-	r.POST("/deletemeigen/:meigen_id/:character_id", handler.DeleteMeigen)
+	auth.POST("/createnickname/:character_id", handler.CreateNickname)
+	auth.POST("/deletenickname/:nickname_id/:character_id", handler.DeleteNickname)
 
-	r.GET("/checker", handler.ShowChecker)
-	r.POST("/checker", handler.ShowResult)
+	auth.POST("/createmeigen/:character_id", handler.CreateMeigen)
+	auth.POST("/deletemeigen/:meigen_id/:character_id", handler.DeleteMeigen)
+
+	auth.GET("/checker", handler.ShowChecker)
+	auth.POST("/checker", handler.ShowResult)
 }
